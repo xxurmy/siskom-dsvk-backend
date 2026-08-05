4069747ff2065d571fe0ad9ed2932cd6f60bbf98
Yokataaaaa <rheynestahan@gmail.com>
2026-08-05 14:17:57 +0700

    penambahan seeder seminar

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
+            KolokiumSeeder::class,
+            SeminarSeeder::class,
         ]);
     }
     }
